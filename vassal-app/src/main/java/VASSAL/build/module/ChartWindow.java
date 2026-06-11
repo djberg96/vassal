bbf4e99c4a86a4a39cb3a10995064f4d09f645dd
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-06-11 14:36:18 -0400

    Refactor chart window state

--- vassal-app/src/main/java/VASSAL/build/module/ChartWindow.java
+++ vassal-app/src/main/java/VASSAL/build/module/ChartWindow.java
@@ -52,7 +52,7 @@ import java.util.List;
 /**
  * A top-level Widget for displaying Charts
  */
-public class ChartWindow extends Widget {
+public final class ChartWindow extends Widget {
   public static final String DESCRIPTION = "description"; //NON-NLS
   public static final String DEPRECATED_NAME = "label"; //$NON-NLS-1$
   public static final String NAME = "name"; //$NON-NLS-1$
@@ -60,11 +60,11 @@ public class ChartWindow extends Widget {
   public static final String TOOLTIP = "tooltip"; //$NON-NLS-1$
   public static final String ICON = "icon"; //$NON-NLS-1$
   public static final String HOTKEY = "hotkey"; //$NON-NLS-1$
-  protected LaunchButton launch;
-  protected JDialog frame;
-  protected Container root;
-  protected String tooltip = ""; //$NON-NLS-1$
-  protected String id;
+  private final LaunchButton launch;
+  private JDialog frame;
+  private Container root;
+  private String tooltip = ""; //$NON-NLS-1$
+  private String id;
 
   public ChartWindow() {
     root = new JPanel();
@@ -236,7 +236,7 @@ public class ChartWindow extends Widget {
     };
   }
 
-  public static class IconConfig implements ConfigurerFactory {
+  public static final class IconConfig implements ConfigurerFactory {
     @Override
     public Configurer getConfigurer(AutoConfigurable c, String key, String name) {
       return new IconConfigurer(key, name, "/images/chart.gif"); //$NON-NLS-1$
