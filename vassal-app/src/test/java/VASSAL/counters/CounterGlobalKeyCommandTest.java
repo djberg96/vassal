dddb938638398c39a12be60eb974da262f862a90
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-06-12 16:53:49 -0400

    Remove unused counter command test imports

--- vassal-app/src/test/java/VASSAL/counters/CounterGlobalKeyCommandTest.java
+++ vassal-app/src/test/java/VASSAL/counters/CounterGlobalKeyCommandTest.java
@@ -19,12 +19,8 @@ package VASSAL.counters;
 
 import VASSAL.configure.PropertyExpression;
 import VASSAL.tools.NamedKeyStroke;
-import VASSAL.tools.icon.IconFactory;
 import java.lang.reflect.InvocationTargetException;
-import javax.swing.ImageIcon;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 public class CounterGlobalKeyCommandTest extends DecoratorTest {
 
