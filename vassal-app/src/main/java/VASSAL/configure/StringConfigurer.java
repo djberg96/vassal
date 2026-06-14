36ed7d589e0edea03a4c726acf3ebb60308c6d4d
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-06-14 11:55:13 -0400

    Parameterize string configurer layer cast

--- vassal-app/src/main/java/VASSAL/configure/StringConfigurer.java
+++ vassal-app/src/main/java/VASSAL/configure/StringConfigurer.java
@@ -215,7 +215,7 @@ public class StringConfigurer extends Configurer {
     @Override
     public void paint(Graphics g, JComponent c) {
       super.paint(g, c);
-      final Component cc = ((JLayer) c).getView();
+      final Component cc = ((JLayer<?>) c).getView();
       if (parent.isHighlighted()) {
         final Dimension d = cc.getSize();
         g.setColor(Color.red);
