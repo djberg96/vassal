9a946d265aad3f19f0bcb561bea2ea91ed6ee51c
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-06-18 15:26:15 -0400

    Use nproc/2 for threads, update build instructions.

--- tools/run-vassal.sh
+++ tools/run-vassal.sh
@@ -11,13 +11,22 @@ else
   MVN="${MVN:-mvn}"
 fi
 
-MAVEN_THREADS="${MAVEN_THREADS:-8}"
+if [[ -n "${JAVA_HOME:-}" ]]; then
+  JAVA="${JAVA:-$JAVA_HOME/bin/java}"
+else
+  JAVA="${JAVA:-java}"
+fi
+
+DEFAULT_MAVEN_THREADS="$(nproc)"
+DEFAULT_MAVEN_THREADS="$(( DEFAULT_MAVEN_THREADS > 1 ? DEFAULT_MAVEN_THREADS / 2 : 1 ))"
+MAVEN_THREADS="${MAVEN_THREADS:-$DEFAULT_MAVEN_THREADS}"
 CLASSPATH_FILE="${TMPDIR:-/tmp}/vassal-app.classpath"
 
+"$MVN" -T "$MAVEN_THREADS" -U -pl vassal-deprecation -am install -DskipTests
 "$MVN" -T "$MAVEN_THREADS" -U -pl vassal-app -am package -DskipTests
 "$MVN" -T "$MAVEN_THREADS" -pl vassal-app dependency:build-classpath \
   "-Dmdep.outputFile=$CLASSPATH_FILE"
 
-exec java -cp \
+exec "$JAVA" -cp \
   "$ROOT/vassal-app/target/classes:$ROOT/vassal-deprecation/target/classes:$(cat "$CLASSPATH_FILE")" \
   VASSAL.launch.ModuleManager "$@"
